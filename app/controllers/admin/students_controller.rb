17ee4d97b3b3b44baf2f50d0ed47a6520acc28c9
lagayaaaron <lagayaaaron@gmail.com>
2023-04-25 10:03:17 +0800

    Fixed before_action:set_student StudentsController

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -1,5 +1,5 @@
 class Admin::StudentsController < ApplicationController
-    before_action :set_student, only: %i[ show edit update destroy ]
+    before_action :set_student, only: %i[show edit update destroy]
     
     def index
         @students = Student.all
