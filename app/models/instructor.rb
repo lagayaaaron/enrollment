0865d385d757b5f081634eeab25fd6dd93b95705
lagayaaaron <lagayaaaron@gmail.com>
2023-04-25 10:50:14 +0800

    instructor Inherited User class

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,4 @@
-class Instructor < ApplicationRecord
+class Instructor < User
   has_one :profile, as: :profileable, dependent: :destroy
   has_many :subject_schedules
   has_many :subjects, through: :subject_schedules
