2d4adddc0465e6cf82b259a9f0f747edbd5ab798
lagayaaaron <lagayaaaron@gmail.com>
2023-04-25 10:02:18 +0800

    Removed belongs_to:user, nested_attribute Admin Model

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,7 +1,4 @@
 class Administrator < ApplicationRecord
-  belongs_to :user, dependent: :destroy
   has_one :profile, as: :profileable, dependent: :destroy
 
-  accepts_nested_attributes_for :user
-  accepts_nested_attributes_for :profile
 end
