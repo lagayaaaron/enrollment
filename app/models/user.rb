3c700fd8ccdf784ebc1aaafb3a6e9336cc795ee8
lagayaaaron <lagayaaaron@gmail.com>
2023-05-03 11:56:03 +0800

    edited is_student function

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
 
   enum :year, %W[1st 2nd 3rd 4th]
   enum :sem, %W[First Second]
+  
   #STI Attribute
   self.inheritance_column = :type
+
   validates :email, uniqueness: true
   #validation of course for student
   validates :course_id, presence: true, if: -> { is_student? }
@@ -20,14 +23,25 @@ class User < ApplicationRecord
 
   searchable do
     integer :course_id
+    # integer :profileable_id
     text :email
+    # join(:name, :prefix => "profile", :target => Profile, :type => :text, :join => { :from => :id, :to => :profileable_id })
+    # join(:gender, :prefix => "profile", :target => Profile, :type => :string, :join => { :from => :id, :to => :profileable_id })
     join(:name, :prefix => "course", :target => Course, :type => :text, :join => { :from => :id, :to => :course_id })
     join(:code, :prefix => "course", :target => Course, :type => :string, :join => { :from => :id, :to => :course_id })
 
   end
 
   def is_student?
-    User.has_role? (:student)
+    self.has_role?(:student)
+  end
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize_to_fit: [100, 100]).processed 
+    else
+      "/default_profile.jpg"
+    end
   end
 
 end
