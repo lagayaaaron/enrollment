0d1746285e56986824387c3fc0386931c18b1484
lagayaaaron <lagayaaaron@gmail.com>
2023-05-09 16:21:37 +0800

    reconfigured checking authorizarition for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery
+    check_authorization unless: :devise_controller?
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     rescue_from CanCan::AccessDenied do |exception|
-        respond_to do |format|
+        if current_user.nil?
+            session[:next] = request.fullpath
+            redirect_to login_url, alert: 'You have to log in to continue.'
+        else
+            respond_to do |format|
             format.json { render nothing: true, status: :not_found }
-            format.html { redirect_to main_app.root_url, notice: exception.message, status: :not_found }
+            format.html { redirect_to main_app.root_url, alert: exception.message }
             format.js   { render nothing: true, status: :not_found }
+            end
         end
     end
 
