12cce84bfa38cca70f6cb3afba541e9c02486ef4
lagayaaaron <lagayaaaron@gmail.com>
2023-05-11 17:16:08 +0800

    Fixed changeset, changes not showing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_075412) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_11_090954) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -163,6 +163,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_075412) do
     t.string "whodunnit"
     t.text "object", limit: 1073741823
     t.datetime "created_at"
+    t.text "object_changes", limit: 1073741823
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
