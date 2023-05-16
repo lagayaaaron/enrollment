a8f8ae5407bad32b0162e797900493cbe5aecbed
lagayaaaron <lagayaaaron@gmail.com>
2023-05-16 09:52:12 +0800

    User can now login with username/email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :course_id, :year, :sem, :avatar])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password])
         devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password, :course_id, :year, :sem, :avatar])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  attr_writer :login
+
   has_one_attached :avatar
 
   enum :year, %W[1st 2nd 3rd 4th]
@@ -14,6 +17,7 @@ class User < ApplicationRecord
   
   #STI Attribute
   self.inheritance_column = :type
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   #validation of course for student
@@ -44,4 +48,17 @@ class User < ApplicationRecord
     end
   end
 
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+  
 end
