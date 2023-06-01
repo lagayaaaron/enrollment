43e0c1badeca173deca44472ba3f422a0801ef5e
lagayaaaron <lagayaaaron@gmail.com>
2023-06-01 15:39:30 +0800

    Fixed error status for forms

--- app/controllers/subject_schedules_controller.rb
+++ app/controllers/subject_schedules_controller.rb
@@ -1,36 +1,39 @@
 class SubjectSchedulesController < ApplicationController
+    
     before_action :set_subject_schedule, only: [:show, :edit, :update, :destroy]
-    # before_action :load_instructor_and_subject
+    before_action :load_instructor_and_subject
     authorize_resource
+
 
     def index
         @subject_schedules = SubjectSchedule.all
-        end
+    end
 
     def show
+
     end
 
     def new
-    @subject_schedule = SubjectSchedule.new
-    @instructors = Instructor.all
-    @subjects = Subject.all
+        @subject_schedule = SubjectSchedule.new
     end
 
     def create
-    @subject_schedule = SubjectSchedule.new(subject_schedule_params)
-
-    if @subject_schedule.save
-        redirect_to @subject_schedule, notice: 'Subject schedule was successfully created.'
-    else
-        @instructors = Instructor.all
-        @subjects = Subject.all
-        render :new
-    end
+        @subject_schedule = SubjectSchedule.new(subject_schedule_params)
+        respond_to do |format|
+            if @subject_schedule.save
+            format.html { redirect_to subject_schedule_url(@subject_schedule), notice: "Schedula was successfully created." }
+            format.json { render :show, status: :created, location: @course }
+            else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @course.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     def edit
-    @instructors = Instructor.all
-    @subjects = Subject.all
+        @instructors = Instructor.all
+        @subjects = Subject.roots 
+        @subsubjects = @subjects.flat_map(&:descendants).reject(&:root?)
     end
 
     def update
@@ -44,17 +47,21 @@ class SubjectSchedulesController < ApplicationController
     end
 
     def destroy
-    @subject_schedule.destroy
-    redirect_to subject_schedules_url, notice: 'Subject schedule was successfully destroyed.'
+        @subject_schedule.destroy
+        redirect_to subject_schedules_url, notice: 'Subject schedule was successfully destroyed.'
     end
 
     private
 
     def set_subject_schedule
-    @subject_schedule = SubjectSchedule.find(params[:id])
+        @subject_schedule = SubjectSchedule.find_by(params[:id])
     end
-
+    def load_instructor_and_subject
+        @instructors = Instructor.all
+        @subjects = Subject.roots 
+        @subsubjects = @subjects.flat_map(&:descendants).reject(&:root?)
+    end
     def subject_schedule_params
-    params.require(:subject_schedule).permit(:subject_id, :instructor_id, :start_time, :end_time, :day_of_week, :location)
+        params.require(:subject_schedule).permit(:subject_id, :instructor_id, :start_time, :end_time, :day_of_week, :location)
     end
 end
--- app/models/subject_schedule.rb
+++ app/models/subject_schedule.rb
@@ -6,9 +6,9 @@ class SubjectSchedule < ApplicationRecord
 
   has_many :admissions
   has_many :students, through: :admissions
-  validates :start_time, :end_time, :day_of_week, :location, presence: true
-  #  validate :schedule_conflict
-
+  validates :subject, :instructor,:location, :day_of_week, presence: true
+  validates :start_time, :end_time, :day_of_week,  presence: true
+  validate :schedule_conflict, if: -> { errors.empty? }
 
   def formatted_start_time
     start_time.strftime("%H:%M")
@@ -16,15 +16,13 @@ class SubjectSchedule < ApplicationRecord
 
   private
 
-  # def schedule_conflict
-  #   if instructor.subject_schedules.present?
-  #     overlaps = instructor.subject_schedules
-  #                       .where(day_of_week: day_of_week)
-  #                       .where.not(id: id)
-  #                       .where('start_time < ? AND end_time > ?', end_time, start_time)
+  def schedule_conflict
+      overlaps = instructor.subject_schedules
+                        .where(day_of_week: day_of_week)
+                        .where.not(id: id)
+                        .where('start_time < ? AND end_time > ?', end_time, start_time)
 
-  #     errors.add(:base, 'Instructor schedule conflict') unless overlaps.empty?
-  #   end
-  # end
+      errors.add(:base, 'Instructor schedule conflict') unless overlaps.empty?
+  end
 
 end
